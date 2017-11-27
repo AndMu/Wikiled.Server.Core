cf288fb66e81f7818f7923a3ae6c63b7f8d6ccf1
Andrius Mudinas <keistokas@gmail.com>
2017-11-27 21:38:16 +0000

    Update

---
+++ src/Wikiled.Server.Core.Testing/Server/CacheHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Moq;
+
+namespace Wikiled.Server.Core.Testing.Server
+{
+    public class CacheHelper
+    {
+        public CacheHelper()
+        {
+            MemoryCache = new Mock<IMemoryCache>();
+            Entry = new Mock<ICacheEntry>();
+            MemoryCache
+                .Setup(m => m.CreateEntry(It.IsAny<object>()))
+                .Returns(Entry.Object);
+
+        }
+
+        public Mock<ICacheEntry> Entry { get; }
+
+        public Mock<IMemoryCache> MemoryCache { get; }
+    }
+}
