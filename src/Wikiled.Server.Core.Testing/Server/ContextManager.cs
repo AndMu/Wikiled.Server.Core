c14bd264e4f0e11baa79fe60d211c878205bf875
Andrius <andrius@wikiled.com>
2017-11-29 12:13:40 +0000

    Improvments to test context

--- src/Wikiled.Server.Core.Testing/Server/ContextManager.cs
+++ src/Wikiled.Server.Core.Testing/Server/ContextManager.cs
@@ -13,7 +13,7 @@ namespace Wikiled.Server.Core.Testing.Server
 {
     public class ContextManager<T> where T : class
     {
-        public ContextManager()
+        public ContextManager(T controller = null)
         {
             ActionArguments = new Dictionary<string, object>();
             ActionDescriptor = new Mock<ActionDescriptor>();
@@ -22,7 +22,13 @@ namespace Wikiled.Server.Core.Testing.Server
             HttpContext = new Mock<HttpContext>();
             Response = new Mock<HttpResponse>();
             HttpContext.Setup(item => item.Response).Returns(Response.Object);
-            Controller = new Mock<T>();
+            Controller = controller;
+            if (controller == null)
+            {
+                ControllerMock = new Mock<T>();
+                Controller = ControllerMock.Object;
+            }
+
             var actionContext = new ActionContext(
                 HttpContext.Object,
                 RouteData.Object,
@@ -32,7 +38,8 @@ namespace Wikiled.Server.Core.Testing.Server
                 actionContext,
                 new List<IFilterMetadata>(),
                 ActionArguments,
-                Controller.Object);
+                Controller);
+            ControllerContext = new Mock<ControllerContext>();
             HttpRequest = new Mock<HttpRequest>();
             HttpContext.Setup(item => item.Request).Returns(HttpRequest.Object);
             RequestDictionary = new HeaderDictionary();
@@ -42,6 +49,10 @@ namespace Wikiled.Server.Core.Testing.Server
             HttpContextAccessor = new Mock<IHttpContextAccessor>();
             HttpContextAccessor.Setup(item => item.HttpContext).Returns(HttpContext.Object);
         }
+
+        public T Controller { get; }
+
+        public Mock<ControllerContext> ControllerContext { get; }
 
         public Mock<HttpResponse> Response { get; }
 
@@ -65,7 +76,7 @@ namespace Wikiled.Server.Core.Testing.Server
 
         public ActionExecutingContext ActionExecutedContext { get; }
 
-        public Mock<T> Controller { get; }
+        public Mock<T> ControllerMock { get; }
 
     }
 }
