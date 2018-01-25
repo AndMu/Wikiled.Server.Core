7226980e7fdcda2e174ef934d02178bfc0a5b0db
Andrius <andrius@wikiled.com>
2018-01-25 12:44:15 +0000

    Change logging

--- src/Wikiled.Server.Core/ActionFilters/RequestValidationAttribute.cs
+++ src/Wikiled.Server.Core/ActionFilters/RequestValidationAttribute.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using NLog;
 using Wikiled.Server.Core.Responses;
 
 namespace Wikiled.Server.Core.ActionFilters
 {
     public class RequestValidationAttribute : ActionFilterAttribute
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (!context.ModelState.IsValid)
             {
-                context.Result = new InvalidViewStateResponse(context.ModelState);
+                logger.Error("Invlid View state detected");
+                var response = new InvalidViewStateResponse(context.ModelState);
+                logger.Error(response.Value);
+                context.Result = response;
             }
 
             base.OnActionExecuting(context);
