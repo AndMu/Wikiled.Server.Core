c64495dff3b01dd676e87678a9326b1c1587149b
Andrius Mudinas <keistokas@gmail.com>
2018-06-20 22:37:45 +0100

    Update packages and add new extensions

---
+++ src/Wikiled.Server.Core/Helpers/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wikiled.Server.Core.Helpers
+{
+    public static class ServiceExtensions
+    {
+        public static TConfig RegisterConfiguration<TConfig>(this IServiceCollection services, IConfiguration configuration, TConfig config) 
+            where TConfig : class
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            if (config == null) throw new ArgumentNullException(nameof(config));
+
+            configuration.Bind(config);
+            services.AddSingleton(config);
+            return config;
+        }
+    }
+}
