d6ef27abb9fc266b4fa12189b343f8006055b016
Andrius Mudinas <andrius@wikiled.com>
2018-06-22 09:42:53 +0100

    Adding exceptin handlig

---
+++ src/Wikiled.Server.Core/Errors/HttpStatusCodeException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace Wikiled.Server.Core.Errors
+{
+    public class HttpStatusCodeException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public string ContentType { get; set; } = @"text/plain";
+
+        public HttpStatusCodeException(int statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public HttpStatusCodeException(int statusCode, string message) : base(message)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public HttpStatusCodeException(int statusCode, Exception inner) : this(statusCode, inner.ToString()) { }
+
+        public HttpStatusCodeException(int statusCode, JObject errorObject)
+            : this(statusCode, errorObject.ToString())
+        {
+            this.ContentType = @"application/json";
+        }
+    }
+}
---
+++ src/Wikiled.Server.Core/Errors/HttpStatusCodeExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Wikiled.Server.Core.Errors
+{
+    public class HttpStatusCodeExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        private readonly ILogger<HttpStatusCodeExceptionMiddleware> logger;
+
+        public HttpStatusCodeExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            this.next = next ?? throw new ArgumentNullException(nameof(next));
+            logger = loggerFactory?.CreateLogger<HttpStatusCodeExceptionMiddleware>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (HttpStatusCodeException ex)
+            {
+                if (context.Response.HasStarted)
+                {
+                    logger.LogWarning("The response has already started, the http status code middleware will not be executed.");
+                    throw;
+                }
+
+                context.Response.Clear();
+                context.Response.StatusCode = ex.StatusCode;
+                context.Response.ContentType = ex.ContentType;
+                await context.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+}
