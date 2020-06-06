585cb170af23e8052db97f179e3bd5054202748b
Andrius Mudinas <keistokas@gmail.com>
2020-06-06 18:30:56 +0100

    Fix to async initialisation reg

--- src/Wikiled.Server.Core/Helpers/AsyncInitializationExtensions.cs
+++ src/Wikiled.Server.Core/Helpers/AsyncInitializationExtensions.cs
@@ -8,8 +8,8 @@ namespace Wikiled.Server.Core.Helpers
         public static IServiceCollection AddAsyncInitializer<T>(this IServiceCollection services)
             where T : class, IAsyncInitialiser
         {
-            services.AddSingleton<T>();
-            return services.AddAsyncFactory<T>((collection, service) => service.InitializeAsync());
+            services.AddSingleton<IAsyncInitialiser, T>();
+            return services.AddAsyncFactory<IAsyncInitialiser>((collection, service) => service.InitializeAsync());
         }
     }
 }
